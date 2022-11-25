d58a8cc7edfee3f2341e98e9da40e8b1d64217ee
Ayona Darshani <ayona.darshani@ogilvy.com>
2022-11-25 16:09:59 +0530

    admin portal login

---
+++ resources/views/admin-portal/login.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Log In | Adminto - Responsive Admin Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{asset('admin/assets/images/favicon.ico')}}">
+
+		<!-- App css -->
+
+		<link href="{{ asset('admin/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-style" />
+
+		<!-- icons -->
+		<link href="{{asset('admin/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="loading authentication-bg authentication-bg-pattern">
+
+        <div class="account-pages my-5">
+            <div class="container">
+
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-4">
+                        <div class="text-center">   
+                            <a href="index.html">
+                                <img src="{{asset('admin/assets/images/logo.png')}}" alt="" height="22" class="mx-auto">
+                            </a>
+                            <p class="text-muted mt-2 mb-4">Laboratory management system for Suhada Laboratories</p>
+
+                        </div>
+                        <div class="card">
+                            <div class="card-body p-4">
+                                
+                                <div class="text-center mb-4">
+                                    <h4 class="text-uppercase mt-0">Sign In</h4>
+                                </div>
+
+                                <form action="#">
+                                    <div class="mb-3">
+                                        <label for="emailaddress" class="form-label">Email address</label>
+                                        <input class="form-control" type="email" id="emailaddress" required="" placeholder="Enter your email">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="password" class="form-label">Password</label>
+                                        <input class="form-control" type="password" required="" id="password" placeholder="Enter your password">
+                                    </div>
+
+                                    
+
+                                    <div class="mb-3 d-grid text-center">
+                                        <button class="btn btn-primary" type="submit"> Log In </button>
+                                    </div>
+                                </form>
+
+                            </div> <!-- end card-body -->
+                        </div>
+                        <!-- end card -->
+
+                        
+                        <!-- end row -->
+
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end page -->
+
+        <!-- Vendor -->
+        <script src="{{asset('admin/assets/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('admin/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('admin/assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{asset('admin/assets/libs/node-waves/waves.min.js')}}"></script>
+        <script src="{{asset('admin/assets/libs/waypoints/lib/jquery.waypoints.min.js')}}"></script>
+        <script src="{{asset('admin/assets/libs/jquery.counterup/jquery.counterup.min.js')}}"></script>
+        <script src="{{asset('admin/assets/libs/feather-icons/feather.min.js')}}"></script>
+
+        <!-- App js -->
+        <script src="{{asset('admin/assets/js/app.min.js')}}"></script>
+        
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/portal-login', function () {
+    return view('admin-portal/login');
+});
